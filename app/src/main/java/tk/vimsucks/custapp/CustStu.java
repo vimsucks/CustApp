5ac0e1f788dbef1c0672de0a31eb00645d7ae2c8
vimsucks <synthetic_ice@yahoo.com>
2016-08-27 22:16:59 +0800

    login complete

--- app/src/main/java/tk/vimsucks/custapp/CustStu.java
+++ app/src/main/java/tk/vimsucks/custapp/CustStu.java
@@ -1,46 +1,67 @@
 package tk.vimsucks.custapp;
 
+import android.app.Activity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
-/**
- * Created by sice on 8/27/16.
- */
 public class CustStu {
 
+    Activity activity;
+    private String username;
+    private String password;
+    public String temp1 = " ";
+    public String temp2 = " ";
+    public String temp3 = " ";
+
     private OkHttpClient httpClient = new OkHttpClient.Builder()
-            .cookieJar(
-                    new CookieJar() {
-                        private final HashMap<String, List<Cookie>> cookieStore = new HashMap<>();
-
-                        @Override
-                        public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
-                            cookieStore.put(url.host(), cookies);
+            .cookieJar(new CookieJar() {
+                //non-persistent CookieJar with an ACCEPT_ALL policy
+                private final Set<Cookie> cookieStore = new LinkedHashSet<>();
+                @Override
+                public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+                    cookieStore.addAll(cookies);
+                }
+                @Override
+                public List<Cookie> loadForRequest(HttpUrl url) {
+                    List<Cookie> matchingCookies = new ArrayList<>();
+                    Iterator<Cookie> it = cookieStore.iterator();
+                    while (it.hasNext()) {
+                        Cookie cookie = it.next();
+                        if (cookie.expiresAt() < System.currentTimeMillis()) {
+                            it.remove();
+                        } else if (cookie.matches(url)) {
+                            matchingCookies.add(cookie);
                         }
-
-                        @Override
-                        public List<Cookie> loadForRequest(HttpUrl url) {
-                            List<Cookie> cookies = cookieStore.get(url.host());
-                            return cookies != null ? cookies : new ArrayList<Cookie>();
-                        }
-                    })
+                    }
+                    return matchingCookies;
+                }                    })
             .build();
     private Request.Builder requestBuilder = new Request.Builder()
             .addHeader("User-Agent", "Mozilla/6.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
     private Request request;
     private Response response;
 
-    public CustStu() {
-
+    public CustStu(String usrName, String passwd) {
+        username = usrName;
+        password = passwd;
     }
 
     private void sysLog(String log) {
@@ -50,12 +71,43 @@ public class CustStu {
     public boolean login() {
         try {
             String loginUrl = "http://jwgl.cust.edu.cn/teachwebsl/login.aspx";
-            request = requestBuilder.url(loginUrl).build();
+            request = requestBuilder
+                    .url(loginUrl)
+                    .build();
             response = httpClient.newCall(request).execute();
-            String html = response.toString();
-            sysLog(html);
+            String html = response.body().string();
+            response.body().close();
+            Document doc = Jsoup.parse(html);
+            String viewState = doc.getElementById("__VIEWSTATE").attr("value");
+            String eventVal = doc.getElementById("__EVENTVALIDATION").attr("value");
+            RequestBody formBody = new FormBody.Builder()
+                    .add("__VIEWSTATE", viewState)
+                    .add("__EVENTVALIDATION", eventVal)
+                    .add("txtUserName", username)
+                    .add("txtPassWord", password)
+                    .add("Button1", "登录")
+                    .build();
+            request = requestBuilder
+                    .url(loginUrl)
+                    .post(formBody)
+                    .build();
+            response = httpClient.newCall(request).execute();
+            response.body().close();
+            String indexUrl = "http://jwgl.cust.edu.cn/teachweb/index1.aspx";
+            request = requestBuilder
+                    .url(indexUrl)
+                    .get()
+                    .build();
+            response = httpClient.newCall(request).execute();
+            html = response.body().string();
+            temp2 = html;
+            response.body().close();
+            doc = Jsoup.parse(html);
+            String stuName = doc.getElementById("StudentNameValueLabel").text();
+            temp1 = stuName;
         } catch (IOException e) {
 
         }
+        return true;
     }
 }
--- app/src/main/java/tk/vimsucks/custapp/MainActivity.java
+++ app/src/main/java/tk/vimsucks/custapp/MainActivity.java
@@ -1,13 +1,74 @@
 package tk.vimsucks.custapp;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustStu stu;
+    Button startButton;
+    ScrollView outputScrollView;
+    TextView outputTextView;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    private Handler outputHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            outputTextView.append((String) msg.obj + "\n");
+            // 在TextView的append后面马上调用fullScroll, 无法滚动到真正的底部
+            // 因为Android下很多函数都是基于消息的，用消息队列来保证同步，所以函数调用多数是异步操作的。
+            // 有消息队列是异步的，消息队列先滚动到底部，然后TextView的append方法显示。所以无法正确滚动到底部。
+            if (outputScrollView != null) {
+                outputScrollView.post(new Runnable() {
+                    public void run() {
+                        outputScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                    }
+                });
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.start_button);
+        outputScrollView = (ScrollView)findViewById(R.id.output_scroll_view);
+        outputTextView = (TextView)findViewById(R.id.output_text_view);
+        usernameEditText = (EditText)findViewById(R.id.username_edit_text);
+        passwordEditText = (EditText)findViewById(R.id.password_edit_text);
+        stu = new CustStu(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+        outputTextView.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+
+    public void start(View view) {
+        new Thread(new Runnable() {
+
+            private void outputMsg(String obj) {
+                Message msg = new Message();
+                msg.obj = obj;
+                outputHandler.sendMessage(msg);
+            }
+
+            @Override
+            public void run() {
+                startButton.setClickable(false);
+                stu.login();
+                outputMsg(stu.temp1);
+                outputMsg(stu.temp2);
+                outputMsg(stu.temp3);
+                startButton.setClickable(true);
+            }
+        }).start();
     }
 }
